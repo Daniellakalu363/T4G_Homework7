7f616934249cec58d80f623f0ea64df9b2b70c24
Daniellakalu363 <daniellakalu4@gmail.com>
2024-06-26 14:33:51 -0700

    add all files

---
+++ src/i.js
@@ -0,0 +1,15 @@
+
+// let age =prompt ("Tell me your age?");
+// let email =prompt("What is your email?");
+
+// alert(`Hi, ${name} you are ${age} years old  and we will keep in touch via your email ${email}👋🏽👋🏽 ` );
+
+// function nameDisplay(){
+//     let name1 = prompt("What is your name?");
+//     hello1.innerHTML = `Hello ${name1}`; // changing the innerhtml of of the element with id hello
+// }
+// let hello1 = document.getElementById('hello'); //selects the element with id hello
+// nameDisplay();
+
+// let name1 = prompt("What is your name?");
+// hello.innerHTML =`Hello ${name1}`; // changing the innnerhtml of the element with id hello
